721275851758fbca3660ac486aa95ceb173bea0a
reallifeliver <pms3300@gmail.com>
2021-05-25 14:41:01 +0900

    rbag to hex

--- build.js
+++ build.js
@@ -62,13 +62,33 @@ function generateStructure(obj) {
   for (let key in obj) {
     if (obj[key].value) {
       const transformedKey = key.replace(/-/g, '_').toUpperCase();
-      rtn[transformedKey] = obj[key].value;
+      rtn[transformedKey] = getHex(obj[key].value);
     } else {
       const transformedKey = key.replace(/ /g, '_');
       rtn[transformedKey] = generateStructure(obj[key]);
     }
   }
   return rtn;
+}
+
+function getHex(rgba) {
+  const match = rgba.match(/^rgba?\(([0-9,/\s]*)\)/i);
+  console.log(rgba, match);
+
+  if (!match || !match[1]) return rgba;
+  const [r, g, b, a] = match[1]
+    .replace(/\s/g, '')
+    .split(',')
+    .map((el) => Number(el));
+
+  if (!!a && a < 1) return rgba; // opacity가 적용되어 있는 경우 rgba 유지
+
+  return `#${toDecimal(r) + toDecimal(g) + toDecimal(b)}`;
+}
+
+function toDecimal(num) {
+  let decimal = num.toString(16);
+  return decimal.length === 1 ? '0' + decimal : decimal;
 }
 
 StyleDictionary.buildAllPlatforms();
